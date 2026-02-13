e8446e02364c5b335a8aa805a65f0b421fd9a41f
Elias-bot-cmd <sikariio1234@gmail.com>
2026-02-13 20:05:50 -0300

    Fix typo in pageTitle for Valentine's config

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Quieres ser mi san valentin? 🤍 🌹",
+    pageTitle: "Quieres ser mi san valetin? 🤍 🌹",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
